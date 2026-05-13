e77584b0ba07ef4179601a7ef16f31abb6c99640
DOliver421-cmd <youpickeddoliver@gmail.com>
2026-05-13 18:52:57 -0400

    Aggressive service worker and cache cleanup

--- frontend/public/clear-sw.js
+++ frontend/public/clear-sw.js
@@ -1,7 +1,13 @@
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.getRegistrations().then(function(registrations) {
     for(let registration of registrations) {
       registration.unregister();
     }
+  });
+  caches.keys().then(function(cacheNames) {
+    cacheNames.forEach(function(cacheName) {
+      caches.delete(cacheName);
+    });
   });
 }
